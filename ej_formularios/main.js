fb20308c4aa43105c11106dbca94369d5675b8ed
daniaccent <danisdnk@gmail.com>
2019-06-07 22:00:37 -0300

    ejerciciohoy

--- ej_formularios/main.js
+++ ej_formularios/main.js
@@ -1,10 +1,26 @@
 let form = document.querySelector("form")
 let input = document.querySelector("input")
+//CHANGE 
+let paises = {
+    ar: ["Buenos Aires", "La Pampa"],
+    ve: ["Margarita", "caracas"],
+    ch: ["Beijing", "Shangai"]
 
+}
 form.addEventListener("submit", handleSubmit)
 function handleSubmit(e) {
     e.preventDefault()
     /* console.log(input.value) */
-console.dir(e.target[0].value)
+    console.dir(form)
+    console.dir(e.target[0].value) // para elementos que no tienen valor,si true o false checked
+    let nombre = e.target[0].trim().value
+
+    console.dir(nombre.length);
+
 }
-/* submit es un evento que se dispara cundo un formulario intenta ser enviado */
+/* submit es un evento que se dispara cundo un formulario intenta ser enviado
+String,length =>Number - la longitud del string
+String.trim() =>String Devuelve strig  pero elimina espacios en blanco para que no cuenten como length  
+{String|Array}.indexOf(X) => Number Retorna la posicion de la instancia del elemento X o -1 si no se encuentra
+{String|Array}.includes()=> Boolen- retorna verdadero o falso dependiendo si X se encuentra o no 
+*/
---
+++ ej_formularios/script.js
@@ -0,0 +1,52 @@
+let form = document.querySelector("form")
+let input = document.querySelector("input")
+
+//1)selecciona el <select>
+let select = document.querySelector("select")
+
+//2) Asignarle un evento change
+select.addEventListener("change", SeleccionCallback)
+
+//CHANGE 
+let paises = {
+    ar: ["Buenos Aires", "La Pampa"],
+    ve: ["Margarita", "caracas"],
+    ch: ["Beijing", "Shangai"]
+
+}
+/* form.addEventListener("submit", handleSubmit)
+
+3 el callback tiene que agarrar el valor del select
+
+ */function SeleccionCallback(e) {
+    e.preventDefault()
+    /* console.log(input.value) */
+    /* console.dir(form) */
+    console.dir(e.target[0].value)
+    console.dir(select.value)
+    let valor = select.selectedIndex
+    let paisesOpcion = paises[valor]
+    if (valor == paisesOpcion) {
+        console.log("es argentina")
+
+    }
+    /* 
+
+    paises.forEach( value => {
+        console.log(value)
+    
+    
+    }); */
+
+
+}
+/* submit es un evento que se dispara cundo un formulario intenta ser enviado
+String,length =>Number - la longitud del string
+String.trim() =>String Devuelve strig  pero elimina espacios en blanco para que no cuenten como length
+
+4) con el valor obtenido buscar el objeto "paises" de la propiedad que le corresponda
+5 si la propiedad no existe se tiene que ver en pantalla un mensaje que diga "tiene que seleccionar un opcion" en color rojo
+6) Si la propiedad existe,tenemos que recorrer el array qye tiene como valor asociado
+7 por cada valor del array se tiene que crear un <option>
+8) arrgegr todos los <option> creados a un nuevo <select>
+9 agregar el select al domZ */
