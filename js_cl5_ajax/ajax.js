42198bc831fa8ccdde323feeaa508c99afe94cda
daniaccent <danisdnk@gmail.com>
2019-06-12 22:03:54 -0300

    clase 6 ajax

--- js_cl5_ajax/ajax.js
+++ js_cl5_ajax/ajax.js
@@ -1,4 +1,16 @@
 let xhr = new XMLHttpRequest
-
+xhr.addEventListener("readystatechange", cambioDeEstado)
+xhr.addEventListener("load", termino)
+xhr.open("GET", "info.txt")
+//http://127.0.0.1:5500/index.html
+xhr.send()
 console.log(xhr)
-xhr.open("GET","info.txt")
+
+function cambioDeEstado() {
+    console.log(xhr)
+}
+function termino() {
+    if (xhr.status == 200) {
+        console.log(xhr.response);
+    }
+}
